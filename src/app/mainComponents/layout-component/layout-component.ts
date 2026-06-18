d54e8d9f974a6a86c21f2faaa7a9e5ede370930d
Dilan-sits <dilan.l@sits.lk>
2026-06-18 12:33:37 +0530

    language change fixed

--- src/app/mainComponents/layout-component/layout-component.ts
+++ src/app/mainComponents/layout-component/layout-component.ts
@@ -21,18 +21,53 @@ export class LayoutComponent implements OnInit {
   activeLang = 'en';
   readonly onImageError = onImageError;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngOnInit() {
-    if (!isPlatformBrowser(this.platformId)) return;
+    if (isPlatformBrowser(this.platformId)) {
+      const savedLang = localStorage.getItem('preferred_lang');
 
-    this.activeLang = getSavedLang();
-    requestGoogleTranslateScript();
+      if (savedLang && savedLang !== 'en') {
+        this.activeLang = savedLang;
+        this.applyGoogleTranslate(savedLang);
+      }
+    }
   }
 
   changeLang(lang: string) {
     this.activeLang = lang;
     localStorage.setItem('preferred_lang', lang);
-    applyGoogleTranslateLang(lang);
+    if (lang === 'en') {
+      this.resetGoogleTranslate();
+      return;
+    }
+
+    this.applyGoogleTranslate(lang);
+  }
+
+  private applyGoogleTranslate(lang: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      const interval = setInterval(() => {
+        const select = document.querySelector('.goog-te-combo') as HTMLSelectElement;
+        if (select) {
+          select.value = lang;
+          select.dispatchEvent(new Event('change'));
+          clearInterval(interval);
+        }
+      }, 300);
+    }
+  }
+
+  private resetGoogleTranslate() {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    document.cookie = 'googtrans=;path=/;domain=' + location.hostname;
+    document.cookie = 'googtrans=;path=/';
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 100);
   }
 }
